5c15fc7c4f19d405c70d6231d269a3197bce7862
muhammad wasi81 <wasiarain81@gmail.com>
2021-02-26 00:48:29 +0500

    Create ChatApp using ChatJs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed.jsx';
+import LoginForm from './components/LoginForm.jsx';
+import './App.css';
 
-function App() {
+const projectID = '145757f1-1b39-45d1-8560-790f5efb9727';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-        
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
